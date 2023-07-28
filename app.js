51b87bc04fe74379596b544f179a6c75d43635ef
elseptiawan <septiawanelshandi@gmail.com>
2023-07-28 08:52:38 +0700

    feat(config): create connection to mongodb

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE_URL);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error);
+});
+
+database.once('connected', () => {
+    console.log("Database Connected");
+});
 
 app.use(express.json());
 
